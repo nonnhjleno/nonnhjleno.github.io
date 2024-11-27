f5e7bf664de5c085e9d09f9fc00e046252d536ea
nonnhjleno <taaaisei999@gmail.com>
2024-11-27 12:30:57 +0900

    RSSをNoodles仕様に変更

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const blog = await getCollection('blog');
   return rss({
-    title: 'Astro学習者 | ブログ',
-    description: 'Astroを学ぶ旅',
+    title: 'Noodles 麺ブログ',
+    description: 'Noodlesは、月に2回サークル活動として、メンバー同士で麺屋に行って食事を行います。その活動の様子をブログやInstagram、X(旧:Twitter)にアップしています。また、ラーメンフェスや学祭での麵屋の出店も行っています。長期休暇期間には流しそうめんや鍋次郎、合宿を行います。',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+    })),
     customData: `<language>ja-jp</language>`,
   });
 }
